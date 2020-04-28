2c231aa5cc58cbc7de29af003e9c37068f1c60aa
ryusuke.miyaji <bluce826@gmail.com>
2020-04-28 12:27:23 +0900

    problem50

--- euler.py
+++ euler.py
@@ -1549,9 +1549,65 @@ def prime_permutations():
         if sequence is not None:
             print(sequence)
 
+# problem50
+
+
+def is_divisible(target, divisors):
+    upper_bound = sqrt(target)
+    for divisor in divisors:
+        if divisor > upper_bound:
+            break
+        if target % divisor == 0:
+            return True
+    return False
+
+
+def prime_numbers(upper_bound):
+    prime_numbers = [2]
+    target = 3
+    while target < upper_bound:
+        if is_divisible(target, prime_numbers) is False:
+            prime_numbers.append(target)
+        target += 2
+    return prime_numbers
+
+
+def assume_max_length(primes):
+    _sum = 0
+    max_prime = primes[-1]
+    for index, prime in enumerate(primes):
+        _sum += prime
+        if _sum > max_prime:
+            return index
+    return len(primes) - 1
+
+
+def sum_of_consecutive_primes(primes, length):
+    start_upper_bound = len(primes) - length
+    max_prime = primes[-1]
+    for start in range(start_upper_bound):
+        _sum = sum(primes[start: start + length])
+        if _sum > max_prime:
+            return None
+        if _sum in primes:
+            return _sum
+    return None
+
+
+def consecutive_prime_sum():
+    UPPER_BOUND = 10 ** 6
+    primes = prime_numbers(UPPER_BOUND)
+    max_length = assume_max_length(primes)
+
+    for length in range(max_length, 21, -1):
+        _sum = sum_of_consecutive_primes(primes, length)
+        if _sum is not None:
+            print('sum of %d consecutive primes: %d' % (length, _sum))
+            break
+
 
 def main():
-    prime_permutations()
+    consecutive_prime_sum()
 
 
 if __name__ == "__main__":
