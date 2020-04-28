5249deba6fc15db8a9bc5122d9a01e12c735e9dc
ryusuke.miyaji <bluce826@gmail.com>
2020-04-28 11:47:05 +0900

    problem47

--- euler.py
+++ euler.py
@@ -1432,9 +1432,66 @@ def goldenbachs_other_conjecture():
             break
         num += 2
 
+# problem47
+
+
+def factoring(num, primes):
+    index = 0
+    factors = set()
+    _sqrt = int(math.sqrt(num))
+    while num != 1:
+        factor = primes[index]
+        if factor > _sqrt:
+            factors.add(num)
+            break
+        if num % factor == 0:
+            num, _ = divmod(num, factor)
+            factors.add(factor)
+        else:
+            index += 1
+    return factors
+
+
+def find_consecutive(primes, length):
+    # between two primes
+    prime_diff = primes[-1] - primes[-2]
+    if prime_diff <= length:
+        return None
+
+    _count = 0
+    for i in range(0, prime_diff):
+        target = primes[-2] + (i + 1)
+        factors = factoring(target, primes)
+        if len(factors) == length:
+            _count += 1
+            if _count == length:
+                return target - 3
+        else:
+            _count = 0
+            # no more n consecutive numbers remain
+            if (primes[-1] - primes[-2]) - (i + 1) < length:
+                break
+    return None
+
+
+def distinct_primes_factors():
+    primes = [2]
+    num = 3
+    LENGTH = 4
+    target = None
+
+    while True:
+        if is_prime(num, primes):
+            primes.append(num)
+            target = find_consecutive(primes, LENGTH)
+            if target is not None:
+                print('answer is %d' % target)
+                break
+        num += 1
+
 
 def main():
-    goldenbachs_other_conjecture()
+    distinct_primes_factors()
 
 
 if __name__ == "__main__":
